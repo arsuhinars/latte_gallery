80bcaea058842c040cb5cfb6e59930e7a41b448d
arsuhinars <arsuhinars@gmail.com>
2024-11-05 11:05:34 +0400

    Services update

--- latte_gallery/accounts/models.py
+++ latte_gallery/accounts/models.py
@@ -15,7 +15,7 @@ class Account(Base):
     __tablename__ = "accounts"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    login: Mapped[str]
+    login: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     name: Mapped[str]
     role: Mapped[Role]
